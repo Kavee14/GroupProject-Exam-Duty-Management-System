7f731041e4b767493a85c85943c90281cf71dab3
Dinekshi99 <sdinekshi@gmail.com>
2024-10-27 23:59:56 +0530

    lecture api

--- ems/routes/api.php
+++ ems/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LecturerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
@@ -10,3 +11,11 @@ Route::get('/user', function (Request $request) {
 
 
 Route::post('login', [UserController::class, 'login']);
+
+Route::prefix('v1')->group(function () {
+    Route::get('lecturers/getAll', [LecturerController::class, 'index']);
+    Route::post('lecturers', [LecturerController::class, 'store']);
+    Route::put('lecturers/edit/{id}', [LecturerController::class, 'update']);
+    Route::delete('lecturers/{id}', [LecturerController::class, 'destroy']);
+    Route::get('lecturers/get/{id}', [LecturerController::class, 'show']);
+});
