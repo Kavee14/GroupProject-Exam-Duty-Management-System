5c47b15458752902e6f3e0b648b0be048c75c6fe
Kavee14 <dissanayake11435@usci.ruh.ac.lk>
2024-10-27 07:08:09 +0530

    Arranged some files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from "./pages/Login/Login";
+import Dashboard from "./pages/Dashboard/Dashboard";
 import Duties from "./pages/Duties/DutiesPage";
+
 
 
 
@@ -10,8 +12,9 @@ function App() {
 
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/duties" element={<Duties />} />
-    </Routes>
+          </Routes>
 
   </Router>;
 }
